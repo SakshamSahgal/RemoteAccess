80e991b6bc61cdc0b105d72544261aec06d0a7e3
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-10-27 00:16:01 +0530

    stream working

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -1,46 +1,76 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
 const app = express();
 const port = 3000;
 const path = require('path');
-const fs = require('fs');
+const puppeteer = require('puppeteer');
+const puppeteerStream = require('puppeteer-stream');
+
+const expressWs = require('express-ws');
+expressWs(app);
 
 
-app.set
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "..", "Static")));
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
 
-app.get('/takescreenshot',async (req,res)=>{
+app.get("/", (req, res) => {
+    res.render(path.join(__dirname, "..", "ClientSide", "index.ejs"))
+})
+
+// WebSocket
+app.ws('/websocket', async (ws, req) => {
     
-    // Launch a headless Chrome browser
-    const browser = await puppeteer.launch();
+    console.log('WebSocket client connected.');
+  
+    let browser;
+    let page;
+    let frameCaptureTimeout;
+  
+    const startStream = async () => {
+      // Start streaming frames
+      const frameInterval = 100;                                                // Adjust the interval (frame rate as needed)
+      const captureFrame = async () => {
+        const screenshotBuffer = await page.screenshot({ type: 'png' });        //capture the frame as a Buffer of PNG data
+        const base64Data = screenshotBuffer.toString('base64');                       // Convert the PNG buffer to base64 data
+        ws.send(base64Data);                                                    // Send the base64 data over the WebSocket connection
+        frameCaptureTimeout = setTimeout(captureFrame, frameInterval);          // Capture the next frame after the frame interval has passed
+      };
+      captureFrame();
+    };
 
-    // Create a new page/tab
-    const page = await browser.newPage();
+    ws.on('connection', async () => {
+        browser = await puppeteer.launch({args: ['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'],});
+        page = await browser.newPage();
+        await page.goto('https://temp-mail.org/en/');
+    })
 
-    // Navigate to a website
-    await page.goto('https://temp-mail.org/en/');
-
-    const Filename = "screenshot.png";
-    const screenshotPath = path.join((__dirname,"..", "Screenshots",Filename));
-
-    // Ensure the directory exists
-    if (!fs.existsSync(screenshotDir)) {
-        fs.mkdirSync(screenshotDir);
-    }
-
-    // Take a screenshot of the page
-    await page.screenshot({ path: screenshotPath });
-
-    console.log(`Screenshot saved to ${screenshotPath}`);
-
-    // Close the browser
-    await browser.close();
-})
-
-app.get("/",(req,res)=>{
-    res.render(path.join(__dirname,"..","ClientSide","index.ejs"))
-})
-
+    ws.on('message', (msg) => {
+      if (msg === 'start') {
+        startStream();
+      }
+      if(msg == 'stop'){
+        if (frameCaptureTimeout) {
+            console.log('Clearing frame capture timeout.');
+            clearTimeout(frameCaptureTimeout);
+            frameCaptureTimeout=null;
+          }
+      }  
+    });
+  
+    ws.on('close', async () => {
+      if (frameCaptureTimeout) {
+        console.log('Clearing frame capture timeout.');
+        clearTimeout(frameCaptureTimeout);
+        frameCaptureTimeout=null;
+      }
+      if (browser) {
+        console.log('Closing browser.');
+        await browser.close();
+        browser = null;
+      }
+      console.log('WebSocket client disconnected.');
+    });
+  });
